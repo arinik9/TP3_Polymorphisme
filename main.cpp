dbd70eaed631ea7948cda86ffbeacf105b15e43f
Nejat <arinik9@hotmail.com>
2014-01-12 21:38:01 +0100

    Update main.cpp
    La commande LOAD est fait!

--- main.cpp
+++ main.cpp
@@ -4,14 +4,65 @@
 #include <vector>
 #include <sstream>
 #include <iostream>
+#include "LectureEcriture.h"
 
 using namespace std;
 
 int main() {
 
 	string commande;
+	std::string token;
+	do {
+	getline(std::cin, commande);
+	std::istringstream ss(commande);
+
+	getline(ss, token, ' ');//Premier Mot
+
+	if(token=="LOAD"){
+		getline(ss, token, '\n');
+		if(token.substr(token.length()-4,4) == ".txt"){
+			LectureEcriture l(token.c_str());//"/home/nejat/Masaüstü/toto.txt"
+
+					while(!l.EstFini()){
+						LigneDeCommande lc;
+						lc=l.ProchainLigne();
+
+					if(!lc.error){
+						if(lc.type=="PL" || lc.type=="C" || lc.type=="R"){
+							cout  << "OK "<< lc.nom;
+							for (std::vector<long>::iterator it = lc.points.begin(); it != lc.points.end();
+										++it)
+									std::cout << ' ' << *it;
+							cout << endl;
+						}
+						if(lc.type=="OA"){
+							cout << "OK " << lc.nom;
+							for (std::set<string>::iterator it=lc.NomObjetUnique.begin(); it!=lc.NomObjetUnique.end(); ++it)
+							   std::cout << ' ' << *it;
+							cout << endl;
+						}
+						if(lc.type=="LIST" || lc.type=="UNDO" || lc.type=="REDO" || lc.type=="CLEAR" || lc.type=="EXIT"){
+							cout  << "OKi "<< lc.type << endl;
+						}
+					}
+					else if(lc.error)
+						cout <<"ERR" << endl;
+				}
+
+
+
+		}
+	}
+	else{
+		getline(ss, token, '\n');
+		cout << "OK " << token << endl;
+	}
+}while(token!="EXIT");
+	/*
+
+	string commande;
 	vector<long> vect;
-	cout << "C:  ";
+	//cout << "C:  ";
 	getline(std::cin, commande);
 	string temp;
 	int counter = 0;
@@ -51,7 +102,7 @@ int main() {
 		std::cout << ' ' << *it;
 
 
-
+*/
 	 /*Figure myfigure;
 	 myfigure.ajouter(typ, nom,vect);
 	 vect.clear();
