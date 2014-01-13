379d99dbf18905e58105641e37c391b28f85386f
Nejat <arinik9@hotmail.com>
2014-01-13 01:34:49 +0100

    Update Figure.h
    l'ajout du MAP

--- Figure.h
+++ Figure.h
@@ -14,17 +14,30 @@
 #include "string"
 #include <vector>
 #include "ElementGeo.h"
-
+#include <map>
+#include <set>
 
 //using namespace std;
 
-class Figure {
+struct Parametres
+{
+	vector<string> objetAgr,elementObjAgr;
+	vector<long> points;
 
+};
+
+class Figure {
+private:
+		//typedef map<std::vector<long>, std::vector<string> > Parametres;
+		typedef map<string, Parametres> ObjetGeo;
+		ObjetGeo stockage;
 public:
 	std::list<ElementGeo*> elements;
 	Figure();
-
-	void ajouter(std::string typeElement,std::string nom, vector<long> vec);
+	void Ajouter(string nomObjet, vector<long> &points); // on va utiliser REFERENCE????
+	void Ajouter(string nomObjet, set<string> QuelobjetAgraget); // on va utiliser REFERENCE????
+	void Ajouter(std::string typeElement,std::string nom, vector<long> vec);
+	void Afficher();
 	virtual ~Figure();
 };
 
